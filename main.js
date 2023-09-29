cb929569dfaddd745c43745d0ef7193ead1c7c32
Mayur Bonde <mayurbonde100@gmail.com>
2023-09-29 17:53:39 +0530

    added a time box

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-alert("This Web Application Is Under Development")
+// alert("This Web Application Is Under Development")
 function maps(){
     window.location.replace("https://www.google.com/maps")
     window.history.go(-1)
 
 }
+let Time = document.querySelector('.time'); 
+function printtime(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    let year = date.getFullYear();
+    let time = date.getTime();
+    Time.innerHTML = day + " / " + month + " / " + year;
+}
+printtime();
+
+    
 function youtube(){
     window.open("https://www.youtube.com/" , "_blank")
 }
